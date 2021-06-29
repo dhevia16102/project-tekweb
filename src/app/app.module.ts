ddde3046a9a432f797a753a789d8ce778b841ab7
Dhevia Leuys Thiaqufyan <dhevia1900016102@webmail.uad.ac.id>
2021-06-29 22:23:20 +0700

    progress 7

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { BerandaComponent } from './utama/beranda/beranda.component';
 import { UtamaComponent } from './utama/content/utama.component';
 import { FooterComponent } from './utama/footer/footer.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { ForgotComponent } from './auth/forgot/forgot.component';
 @NgModule({
   declarations: [
     AppComponent,
     FooterComponent,
     BerandaComponent,
-    UtamaComponent
+    UtamaComponent,
+    LoginComponent,
+    RegisterComponent,
+    ForgotComponent
   ],
   imports: [
     BrowserModule,
